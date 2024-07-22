e115f6b8dd8e8787207bcba8d0cf9fbe1399a0fe
devflinch <72756232+Flinch101@users.noreply.github.com>
2024-07-22 19:39:47 -0300

    Add files via upload

---
+++ base-de-datos.js
@@ -0,0 +1,27 @@
+[
+    [
+        /* Nombre */ "Parlante TG 113 Bluetooth",
+        /* Precio */ "$50.000",
+        [ /* Nombre */ 
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_1.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_2.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_3.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_4.png",
+        ],
+        /* Descripcion */ "- ESPECIFICACIONES TÉCNICAS:<br>-Modelo: TG113.<br>-Material: ABS.<br>-Versión de Bluetooth: 3.0.<br>-Distancia de transmisión: 10m.<br>-Voltaje incorporado: 3.7V.<br>-Capacidad de la batería: 1200ma.<br>-Tamaño de la bocina: 52 mm * 2.<br>-Potencia de la bocina: 42 / 5W * 2.<br>-Distancia de transmisión: 10m.<br>-Voltaje incorporado: 3.7V.<br>-Capacidad de la batería: 1200ma.<br>-Duracion de la carga Volumen normal uso continuo: 5-6 h<br>-Duracion de la carga Volumen maximo uso continuo: 3-4 h<br>-Tamaño de la bocina: 52 mm * 2.<br>-Potencia de la bocina: 4ohms / 5W * 2.<br>-S/N Ratio: =90db<br>-Carga: 5V / 500mah-2A.",
+        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Parlante%20TG%20113!",
+        /* Nuevo/Usado */ "nuevo",
+        /* Cantidad de fotos */ 4
+    ],
+    [
+        /* Nombre */ "Portalampara Adaptador E27",
+        /* Precio */ "$1.500",
+        [ /* Nombre */ 
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_2_1.png"
+        ],
+        /* Descripcion */ "• Alta calidad, durable y estable<br>• Zócalo E27<br>• Adaptador de enchufe para Estados Unidos<br>• Fácil instalación y uso seguro<br>• Resistencia a altas temperaturas<br>• Interruptor de encendido y apagado<br>• Tensión de trabajo: 100V-240V<br>• Material: PVC + Metal<br>• Color: Blanco<br>• Dimensiones: 67 x 47 x 45 mm<br>• Origen: China",
+        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Portalampara%20Adaptador%20E27%20Tecla%20Enchufe%20Velador%20Luz%20Noche%20Color%20Blanco!",
+        /* Nuevo/Usado */ "usado",
+        /* Cantidad de fotos */ 1
+    ],
+]
